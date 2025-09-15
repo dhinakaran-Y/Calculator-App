a83f33a7503d41ab855c8dda1f9169b0125a9247
dhinakaran-Y <dhinakaran.y.dhina14@gmail.com>
2025-09-15 14:30:39 +0530

    npm run build, tailwind.config added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // adjust if you don’t use React/TSX
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
